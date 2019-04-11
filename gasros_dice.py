69b2f50808fa2c146c905ba437dfd88e8b6a4c39
Razvan Marinescu <razvan@csail.mit.edu>
2019-04-11 18:40:18 -0400

    finished generating side-by-side images

--- gasros_dice.py
+++ gasros_dice.py
@@ -24,10 +24,15 @@ manualSegFolder = '/data/vision/polina/projects/wmh/incoming/2018_05_15/gasros_f
 subjList = np.array(np.sort([x for x in os.listdir(outputFolder)]))
 print(os.listdir(outputFolder))
 
-nrSubj = len(subjList)
-# nrSubj = 50
+# nrSubj = len(subjList)
+nrSubj = 200
 
 diceList = np.zeros(nrSubj)
+volAuto = np.zeros(nrSubj)
+volManual = np.zeros(nrSubj)
+
+overlayFld = '%s/data/overlay' % config['subject_data']['output_folder']
+os.system('mkdir -p %s' % overlayFld)
 
 
 for s in range(nrSubj):
@@ -37,10 +42,6 @@ for s in range(nrSubj):
   segManual = '%s/%s/lesionmask/flair/manual/%s_flair_ax_01_seg_01.nii.gz' % (manualSegFolder, subjName, subjName)
   mriOrig = '%s/%s/images/%s_flair_img_brain.nii.gz' % (outputFolder, subjName, subjName)
 
-
-  # print('output_folder', outputFolder)
-  # print(segAuto)
-  # print(segManual)
   overlap = nipype.algorithms.metrics.Overlap()
   overlap.inputs.volume1 = segAuto
   overlap.inputs.volume2 = segManual
@@ -50,31 +51,66 @@ for s in range(nrSubj):
 
   mriArray = nib.load(mriOrig).get_fdata()
   mriArray /= np.max(mriArray)
-  mriSlice = mriArray[:, :, int(mriArray.shape[2] / 2)]
-  print(mriArray.shape, type(mriArray))
+
   segAutoArray = nib.load(segAuto).get_fdata()
   segManualArray = nib.load(segManual).get_fdata()
 
-
+  # compute overlays of MRI with binary segmentation of WMHs
   opacityMri = 0.6
   mriArray *=  opacityMri
+  mriArrayManual = np.copy(mriArray)
+  mriArrayAuto = np.copy(mriArray)
 
-  mriArray[segManualArray == 1] = 1
-  mriArray[segAutoArray == 1] = 0.8
+  mriArrayManual[segManualArray == 1] = 1
+  mriArrayAuto[segAutoArray == 1] = 1
+
+  # compute volumes of WMH segmentations, for both manual seg. and automatic seg.
+  volManual[s] = np.sum(segManualArray)
+  volAuto[s] = np.sum(segAutoArray)
 
   import scipy.misc
 
-  os.system('mkdir -p %s/data/overlay' % config['subject_data']['output_folder'])
-  mriOverlay = '%s/data/overlay/%s_flair_overlay.png' % (config['subject_data']['output_folder'], subjName)
 
-  scipy.misc.imsave(mriOverlay, mriArray[:, :, int(mriArray.shape[2] / 2)])
-  print(np.max(mriArray))
-  print(np.max(segAutoArray))
+  sliceList = range(int(mriArray.shape[2] / 3), 2*int(mriArray.shape[2] / 3))
+  # nrSlices = len(sliceList)
+
+  for sliceNrZ in sliceList:
+    mriOverlayAuto = '%s/%s_sl%.2d_overlay_auto.png' % (overlayFld, subjName, sliceNrZ)
+    mriOverlayManual = '%s/%s_sl%.2d_overlay_manual.png' % (overlayFld, subjName, sliceNrZ)
+    mriOverlay = '%s/%s_sl%.2d_overlay_bck.png' % (overlayFld, subjName, sliceNrZ)
+    mriOverlayTxt = '%s/%s_sl%.2d_overlay.png' % (overlayFld, subjName, sliceNrZ)
+
+    # scipy.misc.imsave(mriOverlayAuto, mriArrayAuto[:, :, sliceNrZ])
+    # scipy.misc.imsave(mriOverlayManual, mriArrayManual[:, :, sliceNrZ])
 
 
+    sideBySideImages = np.concatenate((mriArrayManual[:, :, sliceNrZ], mriArrayAuto[:, :, sliceNrZ]), axis=1)
+    # print(sideBySideImages.shape)
+    # print(mriArrayManual[:, :, sliceNrZ].shape)
+    scipy.misc.imsave(mriOverlay, sideBySideImages)
+    os.system("convert -pointsize 20 -fill white -draw 'text 120,20 \"Manual                  Automatic\" ' %s %s"
+              % (mriOverlay, mriOverlayTxt))
+    os.system('rm %s' % mriOverlay)
 
 
-
-pl.plot(diceList)
+fig1 = pl.figure(1)
+pl.hist(diceList)
+pl.xlabel('dice score')
+fig1.savefig('%s/dice.png' % overlayFld)
 
 print(subjList[:nrSubj][diceList < 0.4])
+
+
+fig2 = pl.figure(2)
+
+# scatter plot manual against automatic volumes
+pl.scatter(volManual, volAuto)
+
+ax = pl.gca()
+xLim = ax.get_xlim()
+pl.plot(xLim, xLim)
+pl.xlabel("manual")
+pl.ylabel("automatic")
+fig2.savefig('%s/scatter_seg.png' % overlayFld)
+
+pl.show()
